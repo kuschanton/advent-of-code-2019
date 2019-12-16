69dcaf1205028e755953c3f20a7b6317dd66f91c
Anton Kushch <kuschanton@gmail.com>
2019-12-16 14:28:51 +0100

    Day 08 ready

---
+++ src/main/kotlin/advent/of/code/day08/Day08.kt
@@ -0,0 +1,52 @@
+package advent.of.code.day08
+
+import advent.of.code.readInputFrom
+
+const val width = 25
+const val height = 6
+
+
+fun main() {
+    val image = readInputFrom("08_1.txt")
+        .map { it.toString().toInt() }
+        .asSequence()
+        .chunked(width * height)
+        .map { it.mapIndexed { index, layerPixel -> Pair(index, layerPixel) }}
+        .flatten()
+        .groupBy({ it.first }) { it.second }
+        .map {
+            it.value.reversed().fold(2) { acc, next ->
+                when(next) {
+                    1 -> 1
+                    0 -> 0
+                    else -> acc
+                }
+            }
+        }
+        .toList()
+
+    drawImage(image, width)
+
+}
+
+fun drawImage(image: List<Int>, width: Int) = image
+    .map { if (it == 0) "0" else " " }
+    .chunked(width)
+    .forEach { println(it.joinToString("")) }
+
+fun part1() {
+    val result = readInputFrom("08_1.txt")
+        .chunked(width * height)
+        .minBy { layer -> layer.count { it == '0' } }
+        ?.fold(Pair(0, 0)) { acc, next ->
+            when(next) {
+                '1' -> Pair(acc.first + 1, acc.second)
+                '2' -> Pair(acc.first, acc.second + 1)
+                else -> acc
+            }
+        }
+        ?.let { it.first * it.second }
+
+    println(result)
+}
+
